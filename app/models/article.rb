b5ae56747fadb1026a881a92d36320ff8109fd7e
PublioScipione <codagnone.francesco@gmail.com>
2021-06-03 08:57:45 +0200

    added validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-
+  validates :title, presence: true, length: { minimum: 6, maximum: 80 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1200 }
 end
